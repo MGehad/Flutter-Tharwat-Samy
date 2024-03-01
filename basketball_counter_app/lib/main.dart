c55792353dd8c7de2a10e4ce0f15007deac0423f
MGehad <134580414+MGehad@users.noreply.github.com>
2024-03-01 11:55:18 +0200

    Added a Basketball Counter App project

---
+++ basketball_counter_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:basketball_counter_app/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PointsCounter());
+}
+
+class PointsCounter extends StatelessWidget {
+  const PointsCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
