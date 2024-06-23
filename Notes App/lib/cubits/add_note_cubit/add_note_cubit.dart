7a486481745b86d36e65546b66f116cd112f5a0b
MGehad <134580414+MGehad@users.noreply.github.com>
2024-06-23 18:14:50 +0300

    Added addNote and deleteNote functions to cubit

--- Notes App/lib/cubits/add_note_cubit/add_note_cubit.dart
+++ Notes App/lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit(super.initialState);
 
-  addNote(NoteModel note){
-
+  addNote(NoteModel note) async {
+    try {
+      emit(AddNoteInitialState());
+      var noteBox = Hive.box<NoteModel>(notesBox);
+      noteBox.add(note);
+      emit(AddNoteSuccessState());
+    } catch (e) {
+      emit(AddNoteFailureState(errMessage: e.toString()));
+    }
   }
 }
--- Notes App/lib/cubits/delete_note_cubit/delete_note_cubit.dart
+++ Notes App/lib/cubits/delete_note_cubit/delete_note_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/models/note_model.dart';
+import 'package:hive/hive.dart';
+import '../../constants.dart';
+import '../../models/note_model.dart';
 import 'delete_note_state.dart';
 
 class DeleteNoteCubit extends Cubit<DeleteNoteState> {
   DeleteNoteCubit(super.initialState);
 
-  deleteNote(NoteModel note) {}
+  deleteNote(NoteModel note) {
+    try {
+      emit(DeleteNoteInitialState());
+      var noteBox = Hive.box<NoteModel>(notesBox);
+      noteBox.delete(note);
+      emit(DeleteNoteSuccessState());
+    } catch (e) {
+      emit(DeleteNoteFailureState(errMessage: e.toString()));
+    }
+  }
 }
