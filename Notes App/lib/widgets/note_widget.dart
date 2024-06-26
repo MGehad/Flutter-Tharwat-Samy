155f32ec2580fdec3e22dc7280801c71767b89c1
MGehad <134580414+MGehad@users.noreply.github.com>
2024-06-26 16:56:26 +0300

    Edit Note Widget

--- Notes App/lib/widgets/note_widget.dart
+++ Notes App/lib/widgets/note_widget.dart
@@ -25,37 +25,46 @@ class _NoteWidgetState extends State<NoteWidget> {
           motion: const ScrollMotion(),
           children: slidableButtons(),
         ),
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20.0),
-          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0),
-          decoration: BoxDecoration(
-            color: Color(widget.note.color),
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 15.0),
-                  child: Text(widget.note.title),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditNoteView(note: widget.note),
+                ));
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            padding: const EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0),
+            decoration: BoxDecoration(
+              color: Color(widget.note.color),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Text(widget.note.title),
+                  ),
+                  titleTextStyle: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                  subtitle: Text(
+                    widget.note.subtitle,
+                    style: const TextStyle(fontSize: 18.0),
+                  ),
                 ),
-                titleTextStyle:
-                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                subtitle: Text(
-                  widget.note.subtitle,
-                  style: const TextStyle(fontSize: 18.0),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.only(right: 18.0, top: 8.0, bottom: 2.0),
-                child: Text(
-                  widget.note.date,
-                  style: const TextStyle(fontSize: 15.0),
-                ),
-              )
-            ],
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 18.0, top: 8.0, bottom: 2.0),
+                  child: Text(
+                    widget.note.date,
+                    style: const TextStyle(fontSize: 15.0),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -64,35 +73,34 @@ class _NoteWidgetState extends State<NoteWidget> {
 
   List<Widget> slidableButtons() {
     return [
-          SlidableAction(
-            onPressed: (context) {
-              widget.note.delete();
-              BlocProvider.of<GetNoteCubit>(context).getAllNotes();
-            },
-            backgroundColor: const Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(15.0),
-                topLeft: Radius.circular(15.0)),
-            icon: Icons.delete,
-            label: 'Delete',
-          ),
-          SlidableAction(
-            onPressed: (context) {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => EditNoteView(
-                  note: widget.note,
-                ),
-              ));
-            },
-            backgroundColor: primaryColor,
-            foregroundColor: Colors.white,
-            borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0)),
-            icon: Icons.edit,
-            label: 'Edit',
-          ),
-        ];
+      SlidableAction(
+        onPressed: (context) {
+          widget.note.delete();
+          BlocProvider.of<GetNoteCubit>(context).getAllNotes();
+        },
+        backgroundColor: const Color(0xFFFE4A49),
+        foregroundColor: Colors.white,
+        borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(15.0), topLeft: Radius.circular(15.0)),
+        icon: Icons.delete,
+        label: 'Delete',
+      ),
+      SlidableAction(
+        onPressed: (context) {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => EditNoteView(
+              note: widget.note,
+            ),
+          ));
+        },
+        backgroundColor: primaryColor,
+        foregroundColor: Colors.white,
+        borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(15.0),
+            bottomRight: Radius.circular(15.0)),
+        icon: Icons.edit,
+        label: 'Edit',
+      ),
+    ];
   }
 }
