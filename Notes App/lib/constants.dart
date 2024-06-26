a967d8285d8f291066453922ea62fdb0bbb44d3a
MGehad <134580414+MGehad@users.noreply.github.com>
2024-06-26 14:16:38 +0300

    Update colors list

--- Notes App/lib/constants.dart
+++ Notes App/lib/constants.dart
@@ -3,12 +3,18 @@ import 'models/color_model.dart';
 
 const Color primaryColor = Color(0xff30CCA7);
 List<ColorModel> colorCircles = [
-  ColorModel(color: Colors.orangeAccent, isSelected: false),
-  ColorModel(color: Colors.indigo, isSelected: false),
-  ColorModel(color: Colors.indigoAccent, isSelected: false),
-  ColorModel(color: Colors.brown, isSelected: false),
-  ColorModel(color: Colors.deepPurple, isSelected: false),
-  ColorModel(color: Colors.green, isSelected: false),
-  ColorModel(color: Colors.redAccent, isSelected: false),
+  ColorModel(color: const Color(0xffFFDA78), isSelected: false),
+  ColorModel(color: const Color(0xff003285), isSelected: false),
+  ColorModel(color: const Color(0xff088395), isSelected: false),
+  ColorModel(color: const Color(0xff37B7C3), isSelected: false),
+  ColorModel(color: const Color(0xffFF7F3E), isSelected: false),
+  ColorModel(color: const Color(0xff836FFF), isSelected: false),
+  ColorModel(color: const Color(0xff15F5BA), isSelected: false),
+  ColorModel(color: const Color(0xffFF204E), isSelected: false),
+  ColorModel(color: const Color(0xff40679E), isSelected: false),
+  ColorModel(color: const Color(0xffFF5733), isSelected: false),
+  ColorModel(color: const Color(0xff6B8A7A), isSelected: false),
+  ColorModel(color: const Color(0xffD63484), isSelected: false),
+  ColorModel(color: const Color(0xff987070), isSelected: false),
 ];
 const String notesBox = "notes_box";
